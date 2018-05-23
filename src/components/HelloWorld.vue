5138c6c1c62d716618c0a74a366db278c5605cb1
Fangzhou1 <13971192686@139.com>
2018-05-23 17:12:17 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$background:rgb(107, 33, 135);
+.hello{
+  background:$background;
+}
+</style>
